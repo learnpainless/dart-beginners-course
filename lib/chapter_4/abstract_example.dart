4db373b577df32e2d15996a8a2e36a10daecadc2
Pawneshwer Gupta <pawneshwergupta@gmail.com>
2023-07-27 16:27:05 +0530

    Added code for Abstraction and Polymorphism

---
+++ lib/chapter_4/abstract_example.dart
@@ -0,0 +1,48 @@
+///Example of Abstraction
+///
+///Also example of Polymorphism
+
+void main(List<String> args) {
+  //Shape shape = Shape(); //Abstract class
+  //shape.sayHello();
+
+  Shape rectangle = Rectangle(1100, 500);
+  Shape circle = Circle(89.6);
+
+  print('Area of Rectangle is ${rectangle.calculateArea()}');
+  print('Area of Circle is ${circle.calculateArea()}');
+
+  rectangle.sayHello();
+  circle.sayHello();
+}
+
+abstract class Shape {
+  double calculateArea();
+
+  void sayHello() {
+    print('Hello from Shape class');
+  }
+}
+
+class Rectangle extends Shape {
+  double width;
+  double height;
+
+  Rectangle(this.height, this.width);
+
+  @override
+  double calculateArea() {
+    return height * width;
+  }
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double calculateArea() {
+    return 3.14 * radius * radius;
+  }
+}
